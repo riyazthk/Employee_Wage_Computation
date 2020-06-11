adec35f3a398e267e98ae5e6f12266c9f63abd23
riyazthk <er.riyaz2507@gmail.com>
2020-06-11 23:09:34 +0530

    ADDED:Add calculate Wages per hour

--- EmWage.java
+++ EmWage.java
@@ -1,11 +1,22 @@
 public class EmWage{
+ int wages=20;
+ int fullday=0;
+  
+public void calculate(int val){
+ switch(val){
+          case 0:
+               fullday=8;
+               break;
+   }
+}
 public static void main(String[] arg){
+EmWage ew=new EmWage();
  int val=(int)(Math.random()*2);
+
      if(val==0){
-          System.out.println("employee is present");
+          ew.calculate(val);
     }else{
           System.out.println("employee is absent");
     }
-
   }
 }
