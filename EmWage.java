06898ac5ba873d995c10d56d37d8309d0ce222df
riyazthk <er.riyaz2507@gmail.com>
2020-06-11 22:50:35 +0530

    To calculate employee is present or not using Math function

---
+++ EmWage.java
@@ -0,0 +1,11 @@
+public class EmWage{
+public static void main(String[] arg){
+ int val=(int)(Math.random()*2);
+     if(val==0){
+          System.out.println("employee is present");
+    }else{
+          System.out.println("employee is absent");
+    }
+
+  }
+}
