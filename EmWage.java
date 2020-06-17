ad6396bcebe52aa865a38c5842fd5890da196792
riyazthk <er.riyaz2507@gmail.com>
2020-06-17 19:34:37 +0530

    ADDED:To save a total wages of a each company

--- EmWage.java
+++ EmWage.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class EmWage {
      int wages;
@@ -11,7 +15,6 @@ public class EmWage {
 	 public int fullday=8;
 	 public int halfday=4;
 	 int totalwages=0;
-
 	EmWage(String company,int wages,int numworkingdays,int workshrpermonth){
 	  this.company=company;
 	  this.wages=wages;
@@ -40,21 +43,30 @@ public class EmWage {
 	          case fulltime:
 	               fullday=8;
 	               calWageMonth(fullday);
-	               System.out.println(company+" " +"total wages for full time :" + totalwages);
+	               //System.out.println(company+" " +"total wages for full time :" + totalwages);
 	               break;
 	          case parttime:
 	               halfday=4;
 	               calWageMonth(parttime);
-	               System.out.println(company +" "+ "total wages for part time time :" + totalwages);
-	               break;
+	              // System.out.println(company +" "+ "total wages for part time time :" + totalwages);
+	               	               break;
 	   }
 	}
 	public static void main(String[] arg){
-		
+	    HashMap< String,Integer> hm=new HashMap<String, Integer>();
+
 		EmWage ew=new EmWage("dmart",30,30,80);
 		ew.calculate();
+		hm.put(ew.company, ew.totalwages);
 		EmWage ew1=new EmWage("reliance",20,15,100);
         ew1.calculate();
-		  }
-    
+		hm.put(ew1.company, ew1.totalwages);
+
+        Set set=hm.entrySet();
+        Iterator iterator =set.iterator();
+        while(iterator.hasNext()) {
+        	Map.Entry mentry=(Map.Entry)iterator.next();
+        	System.out.println(mentry.getKey() + " = "+ mentry.getValue());
+        }
+	}
 }
