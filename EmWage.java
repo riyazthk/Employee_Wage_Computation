ff25f21df58253ce0eb66dd4395200ca336916a6
riyazthk <er.riyaz2507@gmail.com>
2020-06-18 20:06:40 +0530

    ADDED:To add interface for employee wages

--- EmWage.java
+++ EmWage.java
@@ -2,15 +2,22 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.Set;
-
-public class EmWage {
+interface MultipleCompany{
+	public void calWageMonth(int cal);
+	public void employeeWages();
+	public int calculate();
+}
+public class EmWage implements MultipleCompany {
      int wages;
 	 int numworkingdays;
 	 int workshrpermonth;
 	 String company;
 	 String name;
+	 
+	 
 	 public final int fulltime=0;
 	 public final int parttime=1;
 	 public int fullday=8;
@@ -56,35 +63,41 @@ public class EmWage {
 	   }
 	 return totalwages;
 	}
+	public void employeeWages() {
+    	   	
+	}
+	
 	public static void main(String[] arg){
-	    HashMap< String,HashMap<String,Integer>> hm=new HashMap<String,HashMap<String,Integer>>();
+	    HashMap< String,EmWage> hm=new HashMap<String,EmWage>();
 	    Scanner sc=new Scanner(System.in);
-    for(int i=0;i<2;i++) {
-    	System.out.println("enter your company name");
-    	String company=sc.next();
-    	System.out.println("enter company wages");
-    	int wages=sc.nextInt();
-    	System.out.println("enter a working days");
-        int numworkingdays=sc.nextInt();
-        System.out.println("enter a hours per month");
-        int workshrpermonth=sc.nextInt();
-    	for (int j=0;j<2;j++) {
-    	System.out.println("enter your name");
-        String name=sc.next();
-        int totalwages=new EmWage(company,name,wages,numworkingdays,workshrpermonth).calculate();
-    	hm.put(name,new HashMap() {{put(company,totalwages);}});
+	    for(int i=0;i<2;i++) {
+	    	System.out.println("enter your company name");
+	    	String company=sc.next();
+	    	System.out.println("enter company wages");
+	    	int wages=sc.nextInt();
+	    	System.out.println("enter a working days");
+	        int numworkingdays=sc.nextInt();
+	        System.out.println("enter a hours per month");
+	        int workshrpermonth=sc.nextInt();
+	    	for (int j=0;j<2;j++) {
+	    	System.out.println("enter your name");
+	        String name=sc.next();
+	        MultipleCompany multipleCompany=new EmWage(company,name,wages,numworkingdays,workshrpermonth);
+            EmWage emwage=new EmWage(company,name,wages,numworkingdays,workshrpermonth);
+	        multipleCompany.employeeWages();	
+    		emwage.totalwages=multipleCompany.calculate();
+
+        hm.put(name,emwage);
        
     	}        
 
     }
-    System.out.println(hm);
-       Set set=hm.entrySet();
-        Iterator iterator =set.iterator();
-        while(iterator.hasNext()) {
-        	Map.Entry mentry=(Map.Entry)iterator.next();
-        	System.out.println(mentry.getKey() + "  "+ mentry.setValue(mentry.getKey()+" "+mentry.getValue())); 
-        }
-	}
+    Iterator<Entry<String, EmWage>> it=hm.entrySet().iterator();
+    while(it.hasNext()) {
+    	Map.Entry<String, EmWage>li=(Map.Entry<String, EmWage>)it.next();
+    	System.out.println(li.getKey() + " = "+li.getValue().company +" "+ li.getValue().totalwages );
+    }
+  }
 }
     
 
